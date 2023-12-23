9d7c48f3bc0f61bd9957fdf23b7f6d75e052e71d
pierrelouisabraham <pierrelouis.abraham@gmail.com>
2023-12-23 15:25:54 +0100

    push firt js

---
+++ HF/index.js
@@ -0,0 +1,30 @@
+/* async function displayData() {
+    const descriptionSection = document.querySelector(".photographer_section");
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+}; */
+
+
+const nameSkill = document.querySelector('#skill_name');
+const label = document.querySelector('#label');
+const bonus = document.querySelector('#Bonus');
+const description = document.querySelector('#Description');
+
+
+
+function addRow() {
+
+    nameSkill.appendChild()
+    label.appendChild();
+    bonus.appendChild();
+    description.appendChild();
+}
+
+async function init() {
+  
+};
+
+init();
