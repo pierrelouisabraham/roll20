ca3ed7d3be56cd749aa37ff0e65db1cba8274145
pierrelouisabraham <pierrelouis.abraham@gmail.com>
2023-12-30 15:23:41 +0100

    better css

--- HF/index.js
+++ HF/index.js
@@ -1,11 +1,4 @@
-/* async function displayData() {
-    const descriptionSection = document.querySelector(".photographer_section");
-    photographers.forEach((photographer) => {
-        const photographerModel = photographerFactory(photographer);
-        const userCardDOM = photographerModel.getUserCardDOM();
-        photographersSection.appendChild(userCardDOM);
-    });
-}; */
+
 
 
 const nameSkill = document.querySelector('#skill_name');
@@ -16,12 +9,31 @@ const description = document.querySelector('#Description');
 
 
 function addRow() {
+    var sets = ['Force', 'Agilité', 'Endurance', 'Charisme', 'Perception', 'Sagesse', 'Volonté', 'Magie']
+    var inputName = document.createElement('input');
+    inputName.setAttribute('class', 'skill');
+    var inputLabel = document.createElement('select');
+    inputLabel.setAttribute('id', 'skill_label');
 
-    nameSkill.appendChild()
-    label.appendChild();
-    bonus.appendChild();
-    description.appendChild();
+    while (sets.length){
+        var option = new Option(sets.pop());
+        inputLabel.options[inputLabel.options.length] = option;
+    }
+    
+
+
+    var inputBonus = document.createElement('input');
+    inputBonus.setAttribute('class', 'skill');
+    var inputDescription = document.createElement('input');
+    inputDescription.setAttribute('class', 'skill');
+    nameSkill.appendChild(inputName)
+    label.appendChild(inputLabel);
+    bonus.appendChild(inputBonus);
+    description.appendChild(inputDescription);
 }
+
+
+
 
 async function init() {
   
